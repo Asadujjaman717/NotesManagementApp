2595087e56d1dd45675c7c1ad1d8ff78daa86d84
Asadujjaman717 <asadujjaman717@gmail.com>
2025-02-09 23:50:37 +0600

    gitignore added

--- server-site/obj/Debug/net6.0/NotesApi.AssemblyInfo.cs
+++ server-site/obj/Debug/net6.0/NotesApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+427647a634c41124e880185c86781d2874eeb4a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
